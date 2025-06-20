b355b2f7e491084bf7bde99798f0fb50b7e353df
Lucas-Gama360 <lucasrgabrieldev@gmail.com>
2025-06-20 19:53:16 -0300

    ex 001

---
+++ exerciciosjs/ex001/script001.js
@@ -0,0 +1,14 @@
+let vetor = [], i, soma = 0
+    vetor[0] = 1
+    vetor[1] = 0
+    vetor[2] = 5
+    vetor[3] = -2
+    vetor[4] = -5
+    vetor[5] = 7
+        for(i=0; i<=5; i++){
+            soma = vetor[i] + soma
+        }
+    alert("A soma é "+ soma)
+    
+    
+
